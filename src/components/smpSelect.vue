bd9c62ce76c3d8e29f2b095e5d4d0d9549e3abfa
adamgibbons <adam.d.gibbons@gmail.com>
2018-04-14 00:57:41 -0600

    create select component

---
+++ src/components/smpSelect.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="block">
+    <div class="title">
+      {{title}}
+    </div>
+    <div class="select-wrapper">
+      <select
+        @change="setValueByPath({path, value: $event.target.value })"
+        :value="value"
+      >
+        <option></option>
+        <option
+          v-for="(option, index) in options"
+          :value="option.value"
+          :key="index"
+        >{{option.name}}</option>
+      </select>
+    </div>
+    <div class="choose-one">choose one</div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+  props: ['title', 'path', 'options', 'value'],
+  methods: {
+    ...mapActions(['setValueByPath'])
+  }
+}
+</script>
