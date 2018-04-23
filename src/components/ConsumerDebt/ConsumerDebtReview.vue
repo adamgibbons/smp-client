d864e1f5d2353a8f3afc273453f5671f8feb9db2
adamgibbons <adam.d.gibbons@gmail.com>
2018-04-22 22:07:43 -0600

    add consumer debt review vue

--- src/components/ConsumerDebt/ConsumerDebtReview.vue
+++ src/components/ConsumerDebt/ConsumerDebtReview.vue
@@ -1,11 +1,109 @@
 <template>
-  <div>
-    foo
+  <div class="body">
+    <div class="rows">
+      <div
+        class="row"
+        v-for="({ name, averageLoanBalance }, index) in activatedConsumerDebt"
+        :key="index">
+        <!-- <div class="cell name">{{name | prettyName}}</div> -->
+        <div class="cell name">{{name}}</div>
+        <div class="cell amount">${{averageLoanBalance}}</div>
+        <div class="cell edit" @click="edit">
+          <a>edit</a>
+        </div>
+      </div>
+    </div>
+    <div class="page-nav">
+      <button class="addMore" @click="addMore">+ add more</button>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
-  name: 'ConsumerDebtReview'
+  name: 'ConsumerDebtReview',
+  methods: {
+    edit () {
+      this.$router.push({
+        name: 'ConsumerDebtSplash',
+        params: { editingConsumerDebt: true }
+      })
+    },
+    addMore () {
+      this.$router.push({
+        name: 'ConsumerDebtSplash',
+        params: { addingConsumerDebt: true }
+      })
+    }
+  },
+  computed: {
+    ...mapGetters(['consumerDebt']),
+    activatedConsumerDebt () {
+      return Object.entries(this.consumerDebt)
+        .filter(([_, { include }]) => include === true)
+        .map(([name, { include, minMonthlyPayment, averageLoanBalance, interestRate }]) => {
+          return { name, minMonthlyPayment, averageLoanBalance, interestRate }
+        })
+    }
+  },
+  filters: {
+    prettyName: (name) => {
+      return {
+        gymMembership: 'Gym Membership',
+        fuel: 'Fuel',
+        publicTransportation: 'Public Transportation',
+        healthCare: 'Healthcare',
+        phoneInternetCable: 'Phone/Internet/Cable',
+        diningOut: 'Dining Out',
+        shopping: 'Shopping',
+        entertainment: 'Entertainment',
+        childcare: 'Childcare',
+        other: 'Other'
+      }[name]
+    }
+  }
 }
 </script>
+
+<style scoped>
+  button {
+    color: white;
+    border: 1px solid white;
+    padding: 1em;
+    background: transparent;
+  }
+  .rows {
+    display: flex;
+    flex-flow: column nowrap;
+    align-content: flex-start;
+    margin: 1em 0 1em;
+  }
+  .row {
+    border: 1px solid white;
+    border-left: none;
+    border-right: none;
+    display: flex;
+    padding: .9em 0;
+    flex-flow: row nowrap;
+  }
+  .cell {
+    flex: 1;
+    position: relative;
+    left: 10%;
+    font-size: .9em;
+  }
+  .page-nav {
+    text-align: center;
+  }
+  button.addMore {
+    border-radius: 2em;
+    padding: .33em 1em;
+    background: transparent;
+    color: white;
+    border: 2px solid white;
+    font-size: 1em;
+    margin: 0 auto;
+  }
+</style>
