51d38ff79a2d5f741a5ed2ba1c09c254ae5ad1b2
adamgibbons <adam.d.gibbons@gmail.com>
2018-04-29 19:13:23 -0600

    update vehicles edit component

--- src/components/Vehicles/VehiclesEdit.vue
+++ src/components/Vehicles/VehiclesEdit.vue
@@ -23,40 +23,58 @@
       </div>
     </div>
 
-    <!-- year/make/model -->
-    <div class="block">
-      <div class="title">
-        Year/Make/Model
+    <!-- year/make/model and mileage -->
+    <div v-show="form.type === 'Car/SUV'">
+      <!-- year/make/model -->
+      <div class="block">
+        <div class="title">
+          Year/Make/Model
+        </div>
+        <div class="select-wrapper inline" style="margin-left: 0;">
+          <select v-model="form.year">
+            <option></option>
+            <option
+              v-for="(year, index) in years"
+              :key="index"
+              :value="year"
+            >{{year}}</option>
+          </select>
+        </div>
+        <div class="select-wrapper inline">
+          <select v-model="form.make">
+            <option></option>
+            <option
+              v-for="(make, index) in makes"
+              :key="index"
+              :value="make"
+            >{{make}}</option>
+          </select>
+        </div>
+        <div class="select-wrapper inline">
+          <select v-model="form.model">
+            <option></option>
+            <option
+              v-for="(model, index) in models"
+              :key="index"
+              :value="model"
+            >{{model}}</option>
+          </select>
+        </div>
       </div>
-      <div class="select-wrapper inline" style="margin-left: 0;">
-        <select v-model="form.year">
-          <option></option>
-          <option
-            v-for="(year, index) in years"
-            :key="index"
-            :value="year"
-          >{{year}}</option>
-        </select>
-      </div>
-      <div class="select-wrapper inline">
-        <select v-model="form.make">
-          <option></option>
-          <option
-            v-for="(make, index) in makes"
-            :key="index"
-            :value="make"
-          >{{make}}</option>
-        </select>
-      </div>
-      <div class="select-wrapper inline">
-        <select v-model="form.model">
-          <option></option>
-          <option
-            v-for="(model, index) in models"
-            :key="index"
-            :value="model"
-          >{{model}}</option>
-        </select>
+      <!-- mileage -->
+      <div class="block">
+        <div class="title">Mileage</div>
+
+        <div class="control">
+          <div class="select-wrapper">
+            <input
+              type="number"
+              :min="0"
+              :max="250000"
+              v-model="form.mileage"
+            />
+          </div>
+        </div>
       </div>
     </div>
 
@@ -83,114 +101,104 @@
       </div>
     </div>
 
-    <!-- mileage -->
-    <div class="block">
-      <div class="title">Mileage</div>
+    <!-- 9b -->
+    <div v-show="form.ownership === 'Owned - Making payments'">
+      <!-- monthlyPayment -->
+      <div class="block">
+        <div class="title">Monthly Payment (Incl taxes)</div>
 
-      <div class="control">
-        <div class="select-wrapper">
+        <div class="control">
+          <div class="select-wrapper">
+            <span class="unit-symbol">$</span>
+            <input
+              type="number"
+              :min="0"
+              :max="250000"
+              v-model="form.monthlyPayment"
+            />
+          </div>
+        </div>
+      </div>
+
+      <!-- loanBalance -->
+      <div class="block">
+        <div class="title">Loan Balance</div>
+
+        <div class="control">
+          <div class="select-wrapper">
+            <span class="unit-symbol">$</span>
+            <input
+              type="number"
+              :min="0"
+              :max="250000"
+              v-model="form.loanBalance"
+            />
+          </div>
+        </div>
+      </div>
+
+      <!-- loanPaidOffDate -->
+      <div class="block">
+        <div class="title">When does this loan get paid off?</div>
+
+        <div class="control">
+          <div class="form-wrapper">
+            <input
+              type="month"
+              v-model="form.loanPaidOffDate"
+            />
+          </div>
+        </div>
+      </div>
+
+      <!-- loanInterestRate -->
+      <div class="block">
+        <div class="title">What is the interest rate on your loan?</div>
+
+        <div class="slidecontainer">
           <input
-            type="number"
+            class="slider"
+            type="range"
             :min="0"
-            :max="250000"
-            v-model="form.mileage"
+            :max="25"
+            :step="1"
+            v-model="form.loanInterestRate"
           />
+          <label><span>%</span>{{form.loanInterestRate}}</label>
         </div>
       </div>
     </div>
 
-    <!-- monthlyPayment -->
-    <div class="block">
-      <div class="title">Monthly Payment (Incl taxes)</div>
+    <!-- 9c -->
+    <div v-show="form.ownership === 'Leased'">
+      <!-- monthlyLeasePayment -->
+      <div class="block">
+        <div class="title">Monthly Lease Payment</div>
 
-      <div class="control">
-        <div class="select-wrapper">
-          <span class="unit-symbol">$</span>
-          <input
-            type="number"
-            :min="0"
-            :max="250000"
-            v-model="form.monthlyPayment"
-          />
+        <div class="control">
+          <div class="select-wrapper">
+            <span class="unit-symbol">$</span>
+            <input
+              type="number"
+              :min="0"
+              :max="250000"
+              v-model="form.monthlyLeasePayment"
+            />
+          </div>
         </div>
       </div>
-    </div>
 
-    <!-- loanBalance -->
-    <div class="block">
-      <div class="title">Loan Balance</div>
+      <!-- leaseTermEnds -->
+      <div class="block">
+        <div class="title">Lease Term Ends</div>
 
-      <div class="control">
-        <div class="select-wrapper">
-          <span class="unit-symbol">$</span>
-          <input
-            type="number"
-            :min="0"
-            :max="250000"
-            v-model="form.loanBalance"
-          />
-        </div>
-      </div>
-    </div>
-
-    <!-- loanPaidOffDate -->
-    <div class="block">
-      <div class="title">When does this loan get paid off?</div>
-
-      <div class="control">
-        <div class="form-wrapper">
-          <input
-            type="month"
-            v-model="form.loanPaidOffDate"
-          />
-        </div>
-      </div>
-    </div>
-
-    <!-- loanInterestRate -->
-    <div class="block">
-      <div class="title">What is the interest rate on your loan?</div>
-
-      <div class="slidecontainer">
-        <input
-          class="slider"
-          type="range"
-          :min="0"
-          :max="25"
-          :step="1"
-          v-model="form.loanInterestRate"
-        />
-        <label><span>%</span>{{form.loanInterestRate}}</label>
-      </div>
-    </div>
-
-    <!-- monthlyLeasePayment -->
-    <div class="block">
-      <div class="title">Monthly Lease Payment</div>
-
-      <div class="control">
-        <div class="select-wrapper">
-          <span class="unit-symbol">$</span>
-          <input
-            type="number"
-            :min="0"
-            :max="250000"
-            v-model="form.monthlyLeasePayment"
-          />
-        </div>
-      </div>
-    </div>
-
-    <!-- leaseTermEnds -->
-    <div class="block">
-      <div class="title">Lease Term Ends</div>
-
-      <div class="control">
-        <div class="form-wrapper">
-          <input
-            type="month"
-            v-model="form.leaseTermEnds"
-          />
+        <div class="control">
+          <div class="form-wrapper">
+            <input
+              type="month"
+              v-model="form.leaseTermEnds"
+            />
+          </div>
         </div>
       </div>
     </div>
